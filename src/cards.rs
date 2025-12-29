6d08523ddca3a996c45d18f3d7003087a7a8503f
Siddharth Narayan <siddharth.060605@gmail.com>
2025-12-29 05:41:04 -0500

    Get majority of game working. All that's left is actual inference logic

--- src/cards.rs
+++ src/cards.rs
@@ -1,13 +1,14 @@
 use inquire_derive::Selectable;
 use std::fmt::{Display, Formatter};
 
-#[derive(Debug, Copy, Clone, Selectable)]
+#[derive(Eq, Hash, PartialEq, Debug, Copy, Clone, Selectable)]
 pub enum Person {
     White,
     Scarlett,
     Green,
     Mustard,
-    Brown,
+    Peacock,
+    Plum,
 }
 
 impl Person {
@@ -17,7 +18,8 @@ impl Person {
             Self::Scarlett,
             Self::Green,
             Self::Mustard,
-            Self::Brown,
+            Self::Peacock,
+            Self::Plum,
         ]
     }
 }
@@ -25,74 +27,92 @@ impl Person {
 impl Display for Person {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
         match self {
-            Person::White => write!(f, "White"),
-            Person::Scarlett => write!(f, "Scarlett"),
-            Person::Green => write!(f, "Green"),
-            Person::Mustard => write!(f, "Mustard"),
-            Person::Brown => write!(f, "Brown"),
+            Self::White => write!(f, "White"),
+            Self::Scarlett => write!(f, "Scarlett"),
+            Self::Green => write!(f, "Green"),
+            Self::Mustard => write!(f, "Mustard"),
+            Self::Peacock => write!(f, "Peacock"),
+            Self::Plum => write!(f, "Plum"),
         }
     }
 }
 
-#[derive(Debug, Copy, Clone, Selectable)]
+#[derive(Eq, Hash, PartialEq, Debug, Copy, Clone, Selectable)]
 
 pub enum Weapon {
-    White,
-    Scarlett,
-    Green,
-    Mustard,
-    Brown,
+    Candlestick,
+    Dagger,
+    LeadPipe,
+    Revolver,
+    Rope,
+    Wrench,
 }
 impl Weapon {
     pub fn all_variants() -> Vec<Self> {
         vec![
-            Self::White,
-            Self::Scarlett,
-            Self::Green,
-            Self::Mustard,
-            Self::Brown,
+            Self::Candlestick,
+            Self::Dagger,
+            Self::LeadPipe,
+            Self::Revolver,
+            Self::Rope,
+            Self::Wrench,
         ]
     }
 }
 impl Display for Weapon {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
         match self {
-            Weapon::White => write!(f, "White"),
-            Weapon::Scarlett => write!(f, "Scarlett"),
-            Weapon::Green => write!(f, "Green"),
-            Weapon::Mustard => write!(f, "Mustard"),
-            Weapon::Brown => write!(f, "Brown"),
+            Self::Candlestick => write!(f, "Candlestick"),
+            Self::Dagger => write!(f, "Dagger"),
+            Self::LeadPipe => write!(f, "Lead Pipe"),
+            Self::Revolver => write!(f, "Revolver"),
+            Self::Rope => write!(f, "Rope"),
+            Self::Wrench => write!(f, "Wrench"),
         }
     }
 }
 
-#[derive(Debug, Copy, Clone, Selectable)]
+#[derive(Eq, Hash, PartialEq, Debug, Copy, Clone, Selectable)]
 pub enum Location {
-    White,
-    Scarlett,
-    Green,
-    Mustard,
-    Brown,
+    Kitchen,
+    Ballroom,
+    Conservatory,
+    DiningRoom,
+    BilliardRoom,
+    Library,
+    Lounge,
+    Hall,
+    Study,
 }
+
 impl Location {
     pub fn all_variants() -> Vec<Self> {
         vec![
-            Self::White,
-            Self::Scarlett,
-            Self::Green,
-            Self::Mustard,
-            Self::Brown,
+            Self::Kitchen,
+            Self::Ballroom,
+            Self::Conservatory,
+            Self::DiningRoom,
+            Self::BilliardRoom,
+            Self::Library,
+            Self::Lounge,
+            Self::Hall,
+            Self::Study,
         ]
     }
 }
+
 impl Display for Location {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
         match self {
-            Location::White => write!(f, "White"),
-            Location::Scarlett => write!(f, "Scarlett"),
-            Location::Green => write!(f, "Green"),
-            Location::Mustard => write!(f, "Mustard"),
-            Location::Brown => write!(f, "Brown"),
+            Self::Kitchen => write!(f, "Kitchen"),
+            Self::Ballroom => write!(f, "Ballroom"),
+            Self::Conservatory => write!(f, "Conservatory"),
+            Self::DiningRoom => write!(f, "Dining Room"),
+            Self::BilliardRoom => write!(f, "Billiard Room"),
+            Self::Library => write!(f, "Library"),
+            Self::Lounge => write!(f, "Lounge"),
+            Self::Hall => write!(f, "Hall"),
+            Self::Study => write!(f, "Study"),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,152 @@
 mod cards;
 
-use std::fmt::{Display, Formatter};
 use std::collections::HashSet;
+use std::fmt::{Display, Formatter, Pointer};
+use std::hash::Hash;
 
+use inquire::prompt_u32;
 use inquire::{CustomType, InquireError, error::InquireResult, ui::RenderConfig};
+use inquire::{Text, prompt_confirmation};
 use inquire_derive::Selectable;
 
 use cards::{Location, Person, Weapon};
+
+fn clear_terminal() {
+    print!("\x1B[2J\x1B[1;1H");
+}
 
 fn main() -> InquireResult<()> {
     // let inquire_config = RenderConfig::default_colored().with_prompt_prefix( "".into());
 
     // Example using single select
     println!("Select all cards you have");
-    let held_cards: Vec<Card> = Card::multi_select("Cards: ");
+    let held_cards = Card::multi_select("Cards: ");
 
-    held_cards.iter().for_each(|a| {println!("{}", a)} );
+    held_cards.iter().for_each(|a| println!("{}", a));
 
+    let player_count = prompt_u32("How many players other than you are there?").unwrap();
 
-    let player_count: u32 = CustomType::<u32>::new("How many players other than you are there?").prompt()?;
-
-    let players: Vec<Player> = Vec::new();
+    let mut players: Vec<Player> = Vec::new();
     for i in 0..player_count {
+        println!("For player {}, (you're player 0):", i + 1);
+        let mut new_player = Player::new_from_prompt(i + 1);
+
+        new_player.unheld_cards.extend(&held_cards); // Infer: We know they cant hold any cards we hold
+        players.push(new_player);
+    }
+
+    let mut turn = prompt_u32("Who's turn is first?").unwrap();
+    println!("Let the games begin!");
+
+    loop {
+        clear_terminal();
+        println!("It's player {}'s turn. Enter their guess:", turn);
+        let new_guess = Guess::new_from_prompt();
+
+        let mut currently_showing_player = (turn + 1) % (player_count + 1);
+        loop {
+            let prompt = format!(
+                "Does player {} have one of these cards?",
+                currently_showing_player
+            );
+
+            if prompt_confirmation(prompt).unwrap() {
+                if (turn == 0) {
+                    // Infer: We are directly told (not really inferred)
+                    let card = Card::select("What card did the player show you?");
+                    let p: &mut Player = players.get_mut(currently_showing_player as usize).unwrap();
+                    p.held_cards.insert(card);
+                } else {
+                    // Infer: Some player has one of these cards -- We can build a set of potential cards, and find the similarities between shows
+                    // Maybe it's something like the union of all pairwise intersection of guess cards?
+                }
+
+                break;
+            } else {
+                // Infer: This person doesn't have any of these cards
+                let p: &mut Player = players.get_mut(currently_showing_player as usize).unwrap();
+
+                p.unheld_cards.insert(Card::Person(new_guess.person));
+                p.unheld_cards.insert(Card::Weapon(new_guess.weapon));
+                p.unheld_cards.insert(Card::Location(new_guess.location));
+            }
+
+            currently_showing_player += 1;
+            currently_showing_player %= player_count + 1;
+
+            if currently_showing_player == turn {
+                break;
+            }
+
+        }
+
+        players.iter().for_each(|p| {
+            p.display();
+        });
+
+        turn += 1;
+        turn %= player_count + 1;
     }
 
     Ok(())
 }
 
 struct Player {
-    held_cards: HashSet<Card>, // Cards we know for sure they hold
-    unheld_cards: HashSet<Card> // Cards we know for sure they don't hold
+    pub id: u32,
+    pub name: String,
+    pub card_count: u32,
+    pub shown_cards: HashSet<Card>, // Cards we have shown this player before
+    pub held_cards: HashSet<Card>,  // Cards we know for sure they hold
+    pub unheld_cards: HashSet<Card>, // Cards we know for sure they don't hold
 }
 
 impl Player {
-    pub fn new() -> Self {
+    pub fn new_from_prompt(id: u32) -> Self {
+        let name: String = Text::prompt("What is this player's name?".into()).unwrap();
+        let card_count = prompt_u32("How many cards does this player have?").unwrap();
         Player {
+            id: id,
+            name: name,
+            card_count: card_count,
+            shown_cards: HashSet::new(),
             held_cards: HashSet::new(),
-            unheld_cards: HashSet::new()
+            unheld_cards: HashSet::new(),
+        }
+    }
+
+    pub fn display(&self) {
+        println!("Player {} ({})", id, name)
+    }
+}
+
+#[derive(Eq, Hash, PartialEq, Debug, Copy, Clone)]
+struct Guess {
+    person: Person,
+    weapon: Weapon,
+    location: Location,
+}
+
+impl Guess {
+    fn new_from_prompt() -> Self {
+        let person = Person::select("Enter the person for this guess".into())
+            .prompt()
+            .unwrap();
+        let weapon = Weapon::select("Enter the weapon for this guess".into())
+            .prompt()
+            .unwrap();
+        let location = Location::select("Enter the location for this guess".into())
+            .prompt()
+            .unwrap();
+
+        Guess {
+            person: person,
+            weapon: weapon,
+            location: location,
         }
     }
 }
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Eq, Hash, PartialEq, Debug, Copy, Clone)]
 
 enum Card {
     Person(Person),
@@ -52,19 +157,19 @@ enum Card {
 impl Card {
     fn all_cards() -> Vec<Card> {
         let mut cards = Vec::new();
-        
+
         for person in Person::all_variants() {
             cards.push(Card::Person(person));
         }
-        
+
         for weapon in Weapon::all_variants() {
             cards.push(Card::Weapon(weapon));
         }
-        
+
         for location in Location::all_variants() {
             cards.push(Card::Location(location));
         }
-        
+
         cards
     }
 
@@ -74,17 +179,23 @@ impl Card {
             .unwrap()
     }
 
-    fn multi_select(prompt: &str) -> Vec<Card>  {
-        inquire::MultiSelect::new(prompt, Card::all_cards())
+    fn multi_select(prompt: &str) -> HashSet<Card> {
+        let cards = inquire::MultiSelect::new(prompt, Card::all_cards())
             .prompt()
-            .unwrap()
+            .unwrap();
+
+        let mut card_set = HashSet::new();
+        cards.into_iter().for_each(|card| {
+            card_set.insert(card);
+        });
+
+        card_set
     }
 }
 
 impl Display for Card {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
         match self {
-
             Card::Person(p) => p.fmt(f),
             Card::Weapon(w) => w.fmt(f),
             Card::Location(l) => l.fmt(f),
